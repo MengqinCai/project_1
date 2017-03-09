bff365d35e1e35d3365aa82bb276586b10b8ff7d
MengqinCai <mcai1988@unm.edu>
2017-03-09 13:36:48 -0700

    Add files via upload

---
+++ command_cross_method.sh
@@ -0,0 +1,93 @@
+#!/bin/sh
+cd /home/mcai/cs423_project2/out/production/project2_1
+java info.hcstudios.Main -g 100 -s 100 -il 25 -m 0.1 -c NO_CROSS 0.1 -sm TOURNAMENT -lo DAT,MOV,ADD,SUB,MUL,DIV,MOD,JMP,JMZ,JMN,DJN,CMP,SPL,SEQ,SNE,SLT,NOP,STP,LDP -lm DIRECT,IMMEDIATE,INDIRECT_A,INDIRECT_B,PRED_A,PRED_B,POST_A,POST_B -la 10 > output
+cd
+cd /home/mcai/cs423_project2/out/production/project2_2
+java info.hcstudios.Main -g 100 -s 100 -il 25 -m 0.1 -c NO_CROSS 0.1 -sm TOURNAMENT -lo DAT,MOV,ADD,SUB,MUL,DIV,MOD,JMP,JMZ,JMN,DJN,CMP,SPL,SEQ,SNE,SLT,NOP,STP,LDP -lm DIRECT,IMMEDIATE,INDIRECT_A,INDIRECT_B,PRED_A,PRED_B,POST_A,POST_B -la 10 > output
+cd
+cd /home/mcai/cs423_project2/out/production/project2_3
+java info.hcstudios.Main -g 100 -s 100 -il 25 -m 0.1 -c NO_CROSS 0.1 -sm TOURNAMENT -lo DAT,MOV,ADD,SUB,MUL,DIV,MOD,JMP,JMZ,JMN,DJN,CMP,SPL,SEQ,SNE,SLT,NOP,STP,LDP -lm DIRECT,IMMEDIATE,INDIRECT_A,INDIRECT_B,PRED_A,PRED_B,POST_A,POST_B -la 10 > output
+cd
+cd /home/mcai/cs423_project2/out/production/project2_4
+java info.hcstudios.Main -g 100 -s 100 -il 25 -m 0.1 -c NO_CROSS 0.1 -sm TOURNAMENT -lo DAT,MOV,ADD,SUB,MUL,DIV,MOD,JMP,JMZ,JMN,DJN,CMP,SPL,SEQ,SNE,SLT,NOP,STP,LDP -lm DIRECT,IMMEDIATE,INDIRECT_A,INDIRECT_B,PRED_A,PRED_B,POST_A,POST_B -la 10 > output
+cd
+cd /home/mcai/cs423_project2/out/production/project2_5
+java info.hcstudios.Main -g 100 -s 100 -il 25 -m 0.1 -c NO_CROSS 0.1 -sm TOURNAMENT -lo DAT,MOV,ADD,SUB,MUL,DIV,MOD,JMP,JMZ,JMN,DJN,CMP,SPL,SEQ,SNE,SLT,NOP,STP,LDP -lm DIRECT,IMMEDIATE,INDIRECT_A,INDIRECT_B,PRED_A,PRED_B,POST_A,POST_B -la 10 > output
+cd
+cd /home/mcai/cs423_project2/out/production/project2_6
+java info.hcstudios.Main -g 100 -s 100 -il 25 -m 0.1 -c NO_CROSS 0.1 -sm TOURNAMENT -lo DAT,MOV,ADD,SUB,MUL,DIV,MOD,JMP,JMZ,JMN,DJN,CMP,SPL,SEQ,SNE,SLT,NOP,STP,LDP -lm DIRECT,IMMEDIATE,INDIRECT_A,INDIRECT_B,PRED_A,PRED_B,POST_A,POST_B -la 10 > output
+cd
+cd /home/mcai/cs423_project2/out/production/project2_7
+java info.hcstudios.Main -g 100 -s 100 -il 25 -m 0.1 -c NO_CROSS 0.1 -sm TOURNAMENT -lo DAT,MOV,ADD,SUB,MUL,DIV,MOD,JMP,JMZ,JMN,DJN,CMP,SPL,SEQ,SNE,SLT,NOP,STP,LDP -lm DIRECT,IMMEDIATE,INDIRECT_A,INDIRECT_B,PRED_A,PRED_B,POST_A,POST_B -la 10 > output
+cd
+cd /home/mcai/cs423_project2/out/production/project2_8
+java info.hcstudios.Main -g 100 -s 100 -il 25 -m 0.1 -c NO_CROSS 0.1 -sm TOURNAMENT -lo DAT,MOV,ADD,SUB,MUL,DIV,MOD,JMP,JMZ,JMN,DJN,CMP,SPL,SEQ,SNE,SLT,NOP,STP,LDP -lm DIRECT,IMMEDIATE,INDIRECT_A,INDIRECT_B,PRED_A,PRED_B,POST_A,POST_B -la 10 > output
+cd
+cd /home/mcai/cs423_project2/out/production/project2_9
+java info.hcstudios.Main -g 100 -s 100 -il 25 -m 0.1 -c NO_CROSS 0.1 -sm TOURNAMENT -lo DAT,MOV,ADD,SUB,MUL,DIV,MOD,JMP,JMZ,JMN,DJN,CMP,SPL,SEQ,SNE,SLT,NOP,STP,LDP -lm DIRECT,IMMEDIATE,INDIRECT_A,INDIRECT_B,PRED_A,PRED_B,POST_A,POST_B -la 10 > output
+cd
+cd /home/mcai/cs423_project2/out/production/project2_10
+java info.hcstudios.Main -g 100 -s 100 -il 25 -m 0.1 -c NO_CROSS 0.1 -sm TOURNAMENT -lo DAT,MOV,ADD,SUB,MUL,DIV,MOD,JMP,JMZ,JMN,DJN,CMP,SPL,SEQ,SNE,SLT,NOP,STP,LDP -lm DIRECT,IMMEDIATE,INDIRECT_A,INDIRECT_B,PRED_A,PRED_B,POST_A,POST_B -la 10 > output
+cd
+cd /home/mcai/cs423_project2/out/production/project2_11
+java info.hcstudios.Main -g 100 -s 100 -il 25 -m 0.1 -c SINGLE_POINT 0.1 -sm TOURNAMENT -lo DAT,MOV,ADD,SUB,MUL,DIV,MOD,JMP,JMZ,JMN,DJN,CMP,SPL,SEQ,SNE,SLT,NOP,STP,LDP -lm DIRECT,IMMEDIATE,INDIRECT_A,INDIRECT_B,PRED_A,PRED_B,POST_A,POST_B -la 10 > output
+cd
+cd /home/mcai/cs423_project2/out/production/project2_12
+java info.hcstudios.Main -g 100 -s 100 -il 25 -m 0.1 -c SINGLE_POINT 0.1 -sm TOURNAMENT -lo DAT,MOV,ADD,SUB,MUL,DIV,MOD,JMP,JMZ,JMN,DJN,CMP,SPL,SEQ,SNE,SLT,NOP,STP,LDP -lm DIRECT,IMMEDIATE,INDIRECT_A,INDIRECT_B,PRED_A,PRED_B,POST_A,POST_B -la 10 > output
+cd
+cd /home/mcai/cs423_project2/out/production/project2_13
+java info.hcstudios.Main -g 100 -s 100 -il 25 -m 0.1 -c SINGLE_POINT 0.1 -sm TOURNAMENT -lo DAT,MOV,ADD,SUB,MUL,DIV,MOD,JMP,JMZ,JMN,DJN,CMP,SPL,SEQ,SNE,SLT,NOP,STP,LDP -lm DIRECT,IMMEDIATE,INDIRECT_A,INDIRECT_B,PRED_A,PRED_B,POST_A,POST_B -la 10 > output
+cd
+cd /home/mcai/cs423_project2/out/production/project2_14
+java info.hcstudios.Main -g 100 -s 100 -il 25 -m 0.1 -c SINGLE_POINT 0.1 -sm TOURNAMENT -lo DAT,MOV,ADD,SUB,MUL,DIV,MOD,JMP,JMZ,JMN,DJN,CMP,SPL,SEQ,SNE,SLT,NOP,STP,LDP -lm DIRECT,IMMEDIATE,INDIRECT_A,INDIRECT_B,PRED_A,PRED_B,POST_A,POST_B -la 10 > output
+cd
+cd /home/mcai/cs423_project2/out/production/project2_15
+java info.hcstudios.Main -g 100 -s 100 -il 25 -m 0.1 -c SINGLE_POINT 0.1 -sm TOURNAMENT -lo DAT,MOV,ADD,SUB,MUL,DIV,MOD,JMP,JMZ,JMN,DJN,CMP,SPL,SEQ,SNE,SLT,NOP,STP,LDP -lm DIRECT,IMMEDIATE,INDIRECT_A,INDIRECT_B,PRED_A,PRED_B,POST_A,POST_B -la 10 > output
+cd
+cd /home/mcai/cs423_project2/out/production/project2_16
+java info.hcstudios.Main -g 100 -s 100 -il 25 -m 0.1 -c SINGLE_POINT 0.1 -sm TOURNAMENT -lo DAT,MOV,ADD,SUB,MUL,DIV,MOD,JMP,JMZ,JMN,DJN,CMP,SPL,SEQ,SNE,SLT,NOP,STP,LDP -lm DIRECT,IMMEDIATE,INDIRECT_A,INDIRECT_B,PRED_A,PRED_B,POST_A,POST_B -la 10 > output
+cd
+cd /home/mcai/cs423_project2/out/production/project2_17
+java info.hcstudios.Main -g 100 -s 100 -il 25 -m 0.1 -c SINGLE_POINT 0.1 -sm TOURNAMENT -lo DAT,MOV,ADD,SUB,MUL,DIV,MOD,JMP,JMZ,JMN,DJN,CMP,SPL,SEQ,SNE,SLT,NOP,STP,LDP -lm DIRECT,IMMEDIATE,INDIRECT_A,INDIRECT_B,PRED_A,PRED_B,POST_A,POST_B -la 10 > output
+cd
+cd /home/mcai/cs423_project2/out/production/project2_18
+java info.hcstudios.Main -g 100 -s 100 -il 25 -m 0.1 -c SINGLE_POINT 0.1 -sm TOURNAMENT -lo DAT,MOV,ADD,SUB,MUL,DIV,MOD,JMP,JMZ,JMN,DJN,CMP,SPL,SEQ,SNE,SLT,NOP,STP,LDP -lm DIRECT,IMMEDIATE,INDIRECT_A,INDIRECT_B,PRED_A,PRED_B,POST_A,POST_B -la 10 > output
+cd
+cd /home/mcai/cs423_project2/out/production/project2_19
+java info.hcstudios.Main -g 100 -s 100 -il 25 -m 0.1 -c SINGLE_POINT 0.1 -sm TOURNAMENT -lo DAT,MOV,ADD,SUB,MUL,DIV,MOD,JMP,JMZ,JMN,DJN,CMP,SPL,SEQ,SNE,SLT,NOP,STP,LDP -lm DIRECT,IMMEDIATE,INDIRECT_A,INDIRECT_B,PRED_A,PRED_B,POST_A,POST_B -la 10 > output
+cd
+cd /home/mcai/cs423_project2/out/production/project2_20
+java info.hcstudios.Main -g 100 -s 100 -il 25 -m 0.1 -c SINGLE_POINT 0.1 -sm TOURNAMENT -lo DAT,MOV,ADD,SUB,MUL,DIV,MOD,JMP,JMZ,JMN,DJN,CMP,SPL,SEQ,SNE,SLT,NOP,STP,LDP -lm DIRECT,IMMEDIATE,INDIRECT_A,INDIRECT_B,PRED_A,PRED_B,POST_A,POST_B -la 10 > output
+cd
+cd /home/mcai/cs423_project2/out/production/project2_21
+java info.hcstudios.Main -g 100 -s 100 -il 25 -m 0.1 -c UNIFORM 0.1 -sm TOURNAMENT -lo DAT,MOV,ADD,SUB,MUL,DIV,MOD,JMP,JMZ,JMN,DJN,CMP,SPL,SEQ,SNE,SLT,NOP,STP,LDP -lm DIRECT,IMMEDIATE,INDIRECT_A,INDIRECT_B,PRED_A,PRED_B,POST_A,POST_B -la 10 > output
+cd
+cd /home/mcai/cs423_project2/out/production/project2_22
+java info.hcstudios.Main -g 100 -s 100 -il 25 -m 0.1 -c UNIFORM 0.1 -sm TOURNAMENT -lo DAT,MOV,ADD,SUB,MUL,DIV,MOD,JMP,JMZ,JMN,DJN,CMP,SPL,SEQ,SNE,SLT,NOP,STP,LDP -lm DIRECT,IMMEDIATE,INDIRECT_A,INDIRECT_B,PRED_A,PRED_B,POST_A,POST_B -la 10 > output
+cd
+cd /home/mcai/cs423_project2/out/production/project2_23
+java info.hcstudios.Main -g 100 -s 100 -il 25 -m 0.1 -c UNIFORM 0.1 -sm TOURNAMENT -lo DAT,MOV,ADD,SUB,MUL,DIV,MOD,JMP,JMZ,JMN,DJN,CMP,SPL,SEQ,SNE,SLT,NOP,STP,LDP -lm DIRECT,IMMEDIATE,INDIRECT_A,INDIRECT_B,PRED_A,PRED_B,POST_A,POST_B -la 10 > output
+cd
+cd /home/mcai/cs423_project2/out/production/project2_24
+java info.hcstudios.Main -g 100 -s 100 -il 25 -m 0.1 -c UNIFORM 0.1 -sm TOURNAMENT -lo DAT,MOV,ADD,SUB,MUL,DIV,MOD,JMP,JMZ,JMN,DJN,CMP,SPL,SEQ,SNE,SLT,NOP,STP,LDP -lm DIRECT,IMMEDIATE,INDIRECT_A,INDIRECT_B,PRED_A,PRED_B,POST_A,POST_B -la 10 > output
+cd
+cd /home/mcai/cs423_project2/out/production/project2_25
+java info.hcstudios.Main -g 100 -s 100 -il 25 -m 0.1 -c UNIFORM 0.1 -sm TOURNAMENT -lo DAT,MOV,ADD,SUB,MUL,DIV,MOD,JMP,JMZ,JMN,DJN,CMP,SPL,SEQ,SNE,SLT,NOP,STP,LDP -lm DIRECT,IMMEDIATE,INDIRECT_A,INDIRECT_B,PRED_A,PRED_B,POST_A,POST_B -la 10 > output
+cd
+cd /home/mcai/cs423_project2/out/production/project2_26
+java info.hcstudios.Main -g 100 -s 100 -il 25 -m 0.1 -c UNIFORM 0.1 -sm TOURNAMENT -lo DAT,MOV,ADD,SUB,MUL,DIV,MOD,JMP,JMZ,JMN,DJN,CMP,SPL,SEQ,SNE,SLT,NOP,STP,LDP -lm DIRECT,IMMEDIATE,INDIRECT_A,INDIRECT_B,PRED_A,PRED_B,POST_A,POST_B -la 10 > output
+cd
+cd /home/mcai/cs423_project2/out/production/project2_27
+java info.hcstudios.Main -g 100 -s 100 -il 25 -m 0.1 -c UNIFORM 0.1 -sm TOURNAMENT -lo DAT,MOV,ADD,SUB,MUL,DIV,MOD,JMP,JMZ,JMN,DJN,CMP,SPL,SEQ,SNE,SLT,NOP,STP,LDP -lm DIRECT,IMMEDIATE,INDIRECT_A,INDIRECT_B,PRED_A,PRED_B,POST_A,POST_B -la 10 > output
+cd
+cd /home/mcai/cs423_project2/out/production/project2_28
+java info.hcstudios.Main -g 100 -s 100 -il 25 -m 0.1 -c UNIFORM 0.1 -sm TOURNAMENT -lo DAT,MOV,ADD,SUB,MUL,DIV,MOD,JMP,JMZ,JMN,DJN,CMP,SPL,SEQ,SNE,SLT,NOP,STP,LDP -lm DIRECT,IMMEDIATE,INDIRECT_A,INDIRECT_B,PRED_A,PRED_B,POST_A,POST_B -la 10 > output
+cd
+cd /home/mcai/cs423_project2/out/production/project2_29
+java info.hcstudios.Main -g 100 -s 100 -il 25 -m 0.1 -c UNIFORM 0.1 -sm TOURNAMENT -lo DAT,MOV,ADD,SUB,MUL,DIV,MOD,JMP,JMZ,JMN,DJN,CMP,SPL,SEQ,SNE,SLT,NOP,STP,LDP -lm DIRECT,IMMEDIATE,INDIRECT_A,INDIRECT_B,PRED_A,PRED_B,POST_A,POST_B -la 10 > output
+cd
+cd /home/mcai/cs423_project2/out/production/project2_30
+java info.hcstudios.Main -g 100 -s 100 -il 25 -m 0.1 -c UNIFORM 0.1 -sm TOURNAMENT -lo DAT,MOV,ADD,SUB,MUL,DIV,MOD,JMP,JMZ,JMN,DJN,CMP,SPL,SEQ,SNE,SLT,NOP,STP,LDP -lm DIRECT,IMMEDIATE,INDIRECT_A,INDIRECT_B,PRED_A,PRED_B,POST_A,POST_B -la 10 > output
+cd
+
+
